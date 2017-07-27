8ac0df4c08d4c521d1cd303c3e4c20b5abb02845
Rami Bououni <rami.bououni@gmail.com>
2017-07-26 17:07:25 -0700

    finished setting up the constring and the ajax method

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -11,13 +11,22 @@ var app = app || {};
     //       Don't forget to remove the headers from our request - we're no longer using a token on the
     //       client side of our app, our new proxyGitHub function will be handling the token using our
     //       new environment variable!
-    $.ajax({
-      url: `https://api.github.com/user/repos`,
-      type: 'GET',
-      headers: {'Authorization': `token ${githubToken}`}
+    // $.ajax({
+    //   url: `https://api.github.com/user/repos`,
+    //   type: 'GET',
+    //   // headers: {'Authorization': `token ${githubToken}`}
+    // })
+
+    $.get('/github', function(request, response) {
+      (requestProxy({
+        url: `https://api.github.com/${request.params[0]}`,
+        // headers: {
+        //   Authorization: `token ${githubToken}`
+        // }
+      }))(request, response)
     })
-    .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
-    .then(callback);
+      .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
+      .then(callback);
   };
 
   repos.with = attr => repos.all.filter(repo => repo[attr]);
